90a5eb290a37d00fcb9363901de6c6f43f2764e9
JyotiBikashPradhan <bikashpradhan8249@mail.com>
2023-05-02 21:34:09 +0530

    findMinMaxComment

--- src/main/java/arrays/FindMinMax.java
+++ src/main/java/arrays/FindMinMax.java
@@ -2,6 +2,26 @@ package arrays;
 
 public class FindMinMax {
 
+
+/**    1,2,3,4,75,9
+ * min =1
+ * max =1
+ * 1<2
+ * 1<3
+ * 1<4
+ * 1<75
+ * 1<9
+ * min=1
+
+ * 1>2 max=2
+ * 2>3 max=3
+ * 3>4 max=4
+ * 4>75 max=75
+ * 75>9 max=75
+ *
+ * 1,75
+ **/
+
     public int[] solution(int[] arr){
 
         int[] res=new int[2];
